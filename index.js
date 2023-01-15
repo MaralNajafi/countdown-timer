1de54d640cfaa1883b03add1ece98e093b328aac
MaralNajafi <marawlnajafi@gmail.com>
2023-01-15 16:30:12 +0330

    created DOM variables

---
+++ index.js
@@ -0,0 +1,6 @@
+//Countdown Timer Project
+
+//Variables
+const DOMminutes = document.querySelector("#minutes");
+const DOMseconds = document.querySelector("#seconds");
+const startBtn = document.querySelector("#startBtn");
