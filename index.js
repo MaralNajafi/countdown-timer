f74d403a929c456bb391c805ec72b3144a5a470a
MaralNajafi <marawlnajafi@gmail.com>
2023-01-15 17:38:14 +0330

    created timeDisplay function and used the function inside start function

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ let countdownInterval;
 
 
 //functions
+
+// time display function
+function timeDisplay(inputTime, outputTime) {
+    if (inputTime >= 10) {
+        outputTime.textContent = inputTime;
+    }else{
+        outputTime.textContent = `0${inputTime}`;
+    }
+}
+
+//start function
 function startFn() {
     countdownInterval = setInterval(() =>{
         --givenSeconds;
         minutes = Math.floor(givenSeconds / secPerMin);
         seconds = Math.floor(givenSeconds % secPerMin);
-        DOMminutes.textContent = minutes;
-        DOMseconds.textContent = seconds;
+        timeDisplay(minutes, DOMminutes)
+        timeDisplay(seconds, DOMseconds)
         
         if (minutes === 0 && seconds === 0) {
             clearInterval(countdownInterval);
