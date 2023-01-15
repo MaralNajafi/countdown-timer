68f85b01e7fefde6e6983f87bdc2d2fc3406d0b0
MaralNajafi <marawlnajafi@gmail.com>
2023-01-15 16:51:23 +0330

    created the start function and essential variables

--- index.js
+++ index.js
@@ -4,3 +4,22 @@
 const DOMminutes = document.querySelector("#minutes");
 const DOMseconds = document.querySelector("#seconds");
 const startBtn = document.querySelector("#startBtn");
+
+let secPerMin = 60;
+let givenMinute = 2;
+let givenSeconds = secPerMin * givenMinute;
+let countdownInterval;
+
+function startFn() {
+    countdownInterval = setInterval(() =>{
+        --givenSeconds;
+        let minutes = Math.floor(givenSeconds / secPerMin);
+        let seconds = Math.floor(givenSeconds % secPerMin);
+    
+        DOMminutes.textContent = minutes;
+        DOMseconds.textContent = seconds;
+        
+    }, 1000)
+}
+
+startBtn.addEventListener("click", startFn)
