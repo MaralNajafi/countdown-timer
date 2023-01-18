cc51539c1c61aa459d31b9669f8664d284c36296
MaralNajafi <marawlnajafi@gmail.com>
2023-01-18 20:09:42 +0330

    minute input validation

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const clearBtn = document.querySelector("#clearBtn");
 
 
 let secPerMin = 60;
-let givenMinute = 2;
+let givenMinute;
 let givenSeconds;
+let DOMminutesValue;
 let minutes;
 let seconds;
 let countdownInterval;
@@ -22,6 +23,7 @@ const displayNone = "d-none";
 
 //default states
 clearBtn.disabled = true;
+startBtn.disabled = true;
 
 //functions
 
@@ -34,25 +36,43 @@ function timeDisplay(inputTime, outputTime) {
     }
 };
 
+//time validation
+function timeValidator() {
+    DOMminutes.value = DOMminutes.value.replace(/[^0-9]/g, "");
+    DOMminutesValue = +DOMminutes.value;
+    if (DOMminutesValue === 0) {
+        startBtn.disabled = true;
+    }else{
+        startBtn.disabled = false;
+    }
+
+    /* if (DOMminutesValue > 60) {
+        DOMminutes.value = 60;
+    } */
+}
 
 //set time function
 function setTime() {
     DOMminutes.removeAttribute("readonly");
     DOMminutes.classList.remove("edit-time");
+    
 }
 
 //start function
 function startFn() {
 
+    DOMminutes.setAttribute("readonly", "")
     clearBtn.disabled = false;
-
-    DOMminutes.setAttribute("readonly", true);
     DOMminutes.classList.remove("edit-time");
     DOMcountdown.classList.remove("times-up");
     startBtn.classList.add(displayNone);
     resetBtn.classList.remove(displayNone);
 
+    DOMminutesValue = +DOMminutes.value;
+    givenMinute = DOMminutesValue;
     givenSeconds = secPerMin * givenMinute;
+    timeDisplay(givenMinute, DOMminutes)
+
     countdownInterval = setInterval(() =>{
         --givenSeconds;
         minutes = Math.floor(givenSeconds / secPerMin);
@@ -73,7 +93,7 @@ function clearFn() {
 
     clearBtn.disabled = true;
     
-    DOMminutes.setAttribute("readonly", true);
+    DOMminutes.setAttribute("readonly", false);
     DOMminutes.classList.add("edit-time");
     DOMcountdown.classList.remove("times-up");
     startBtn.classList.remove(displayNone);
@@ -98,6 +118,7 @@ function resetFn() {
 
 
 DOMminutes.addEventListener("focus", setTime)
+DOMminutes.addEventListener("input", timeValidator)
 startBtn.addEventListener("click", startFn);
 resetBtn.addEventListener("click", resetFn);
 clearBtn.addEventListener("click", clearFn);
